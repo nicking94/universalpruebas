3af2252d464089f7e25088dd0323c9b1519a425a
Nicolas Vallejo <nicolas.vallejo@novexisconsulting.com>
2025-05-19 17:52:47 -0300

    fix trials v2

--- app/lib/constants/constants.ts
+++ app/lib/constants/constants.ts
@@ -1,12 +1,12 @@
 export const USERS = [
   {
-    username: process.env.NEXT_PUBLIC_LOGIN_USERNAME_1 || "admin",
-    password: process.env.NEXT_PUBLIC_LOGIN_PASSWORD_1 || "admin",
+    username: process.env.NEXT_PUBLIC_LOGIN_USERNAME_2 || "demo",
+    password: process.env.NEXT_PUBLIC_LOGIN_PASSWORD_2 || "demo",
     id: 1,
   },
   {
-    username: process.env.NEXT_PUBLIC_LOGIN_USERNAME_2 || "demo",
-    password: process.env.NEXT_PUBLIC_LOGIN_PASSWORD_2 || "demo",
+    username: process.env.NEXT_PUBLIC_LOGIN_USERNAME_1 || "admin",
+    password: process.env.NEXT_PUBLIC_LOGIN_PASSWORD_1 || "admin",
     id: 2,
   },
 ];
